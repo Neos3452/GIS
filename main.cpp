869368aec109da12f071e051709f55e2309287f8
Michał Dębski <debski.mi.zd@gmail.com>
2016-05-02 11:20:31 +0200

    Command line tool for fiding SCC
    You can check graph with following format:
    First 2 integers (V E) which indicates th enumber of vertices and edges
    respectively. Then E number of times two integeres (v1 v2), which indicate that
    there is an edge from v1 to v2. Vertices are indexed from 1 to V.
    Example graph which has three vertices which form a cycle:
    3 3 (3 vertices, 3 edges)
    1 2 (edge from 1 to 2)
    2 3 (edge from 2 to 3)
    3 1 (edge from 3 to 1)
    
    You can pipe input from file or echo.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,56 @@
+#include "Graph.h"
+
 #include <iostream>
 
-int main(int argsc, char* argv[]) {
-    (void(argsc));
-    (void(argv));
-    std::cout << "Hello Graph!" << std::endl;
+void printErrorMsg(const std::ios_base::failure &fail)
+{
+    std::cerr << "Error while processing input (" << fail.code() << "): " << fail.what() << std::endl;
+}
+
+int main()
+{
+    std::cin.sync_with_stdio(false);
+    std::cin.exceptions(std::istream::badbit | std::istream::failbit | std::istream::eofbit);
+    try {
+        while(true) {
+            size_t verticesCount;
+            std::cin >> verticesCount;
+            gis::Graph g;
+            g.vertices.reserve(verticesCount);
+            unsigned id = 1;
+            std::generate_n(std::back_inserter(g.vertices), verticesCount, [&id](){
+               return gis::Vertex(std::to_string(id++));
+            });
+            try {
+                size_t edgesCount;
+                std::cin >> edgesCount;
+                for (size_t i = 0; i < edgesCount; ++i) {
+                    size_t from, to;
+                    std::cin >> from >> to;
+                    if (from > verticesCount || to > verticesCount || from < 1 || to < 1) {
+                        throw std::ios_base::failure("Vertex index out of bounds");
+                    }
+                    g.vertices[(from-1)].neighbours.emplace_back(g.vertices[(to-1)]);
+                }
+                const auto scc = g.findStronglyConntectedComponents();
+                for (const auto &comp : scc) {
+                    std::cout << "{";
+                    for (const gis::Vertex &v : comp) {
+                        std::cout << v.label << ",";
+                    }
+                    std::cout << "}, ";
+                }
+                std::cout << std::endl;
+            } catch (const std::ios_base::failure &fail) {
+                // TODO: actually exception message is very poor error message
+                printErrorMsg(fail);
+                break;
+            }
+        }
+    } catch (const std::ios_base::failure &fail) {
+        if (!std::cin.eof()) {
+            printErrorMsg(fail);
+        }
+    }
 }
 
